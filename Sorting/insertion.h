8b11f69d5f53f556d9fe82e1396fd1adf17f39fc
Hoang Van Loc <loc.hoang@olli-ai.com>
2021-07-03 14:36:24 +0700

    update source code for insertion sort

---
+++ Sorting/insertion.h
@@ -0,0 +1,59 @@
+#ifndef INSERTION_H
+#define INSERTION_H
+#include <iostream>
+
+template <typename T>
+int non_decreasing_insertion_sort(T *head, int size) 
+{
+    if (head == NULL) {
+        return -1;
+    }
+
+    if (size < 2) {
+        return 0;
+    }
+
+    for (int j = 1; j < size; j++) {
+        T key = head[j];
+        int i = j - 1;
+        while (i >= 0) {
+            if (head[i] > key) {
+                head[i + 1] = head[i];
+                i--;
+            } else {
+                break;
+            } 
+        }
+        head[i + 1] = key;
+    } 
+    return 0;
+}
+
+template <typename T>
+int non_increasing_insertion_sort(T *head, int size)
+{
+    if (head == NULL) {
+        return -1;
+    }
+
+    if (size < 2) {
+        return 0;
+    }
+
+    for (int j = 1; j < size; j++) {
+        T key = head[j];
+        int i = j - 1;
+        while (i >= 0) {
+            if (head[i] < key) {
+                head[i + 1] = head[i];
+                i--;
+            } else {
+                break;
+            } 
+        }
+        head[i + 1] = key;
+    } 
+    return 0;
+
+}
+#endif /* INSERTION_H */
---
+++ Sorting/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+#ifdef USE_INSERTION_SORT
+#include "insertion.h"
+#endif
+
+void print_array(int* arr, int size) 
+{
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int non_decreasing_sort(int *arr, int size)
+{
+#ifdef USE_INSERTION_SORT
+    cout << "************ USE INSERTION ****************" << endl;
+    return non_decreasing_insertion_sort(arr, size);
+#endif /* USE_INSERTION_SORT */
+}
+
+int non_increasing_sort(int *arr, int size)
+{
+#ifdef USE_INSERTION_SORT
+    cout << "************ USE INSERTION ****************" << endl;
+    return non_increasing_insertion_sort(arr, size);
+#endif /* USE_INSERTION_SORT */
+}
+
+void run_all_test()
+{
+    int arr[] = {31, 41, 59, 26, 41, 58};
+    int size = sizeof(arr) / sizeof(int);
+    cout << "TEST 1" << endl;
+    cout << "Array before sort: ";
+    print_array(arr, size);
+    non_decreasing_sort(arr, size);
+    cout << "Array after sort: ";
+    print_array(arr, size);
+}
+
+int main(int argc, char * argv[]) {
+    int arr[] = {5, 2, 4, 6, 1, 3};
+    int size = sizeof(arr) / sizeof(int);
+    cout << "Array before sort: ";
+    print_array(arr, size);
+    int ret = non_decreasing_sort(arr, size);
+    if (ret != 0) {
+        cout << "Error while sort the array" << endl;
+    } else {
+        cout << "Array after sort: ";
+        print_array(arr, size);
+    }
+    int arr_2[] = {5, 2, 4, 6, 1, 3};
+    ret = non_increasing_sort(arr_2, size);
+    cout << "Non increasing sort: ";
+    if (ret != 0) {
+        cout << "Error while sort the array" << endl;
+    } else {
+        print_array(arr_2, size);
+    }
+    run_all_test();
+    return 0;
+}
